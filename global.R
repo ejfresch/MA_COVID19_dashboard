2a38e281f1a29be8267c4f8b44aeb23dff90bdc2
Luca Freschi <l.freschi@gmail.com>
2020-04-08 23:27:16 -0400

    Fixed bug with map (was not loading properly when the app was starting); the data are recalculated only when the wikipedia page is updated.

--- global.R
+++ global.R
@@ -7,6 +7,10 @@ library(dplyr)
 library(broom)
 library(mapproj)
 library(reshape2)
+library(digest)
+library(lubridate)
+
+source("get_data_from_wikipedia.R",local=TRUE)
 
 #shapes polygons for leaflet
 ma_spdf = geojson_read("data/cb_2018_ma_county_5m.json",  what = "sp")
@@ -15,135 +19,20 @@ ma_spdf = geojson_read("data/cb_2018_ma_county_5m.json",  what = "sp")
 # Download the data from Wikipedia
 library(rvest)    
 url = "https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_Massachusetts"
-raw_data_wiki = url %>% 
+r_data_wiki = url %>% 
   read_html() 
 
-raw_data= raw_data_wiki %>% 
-  html_node(xpath = '/html/body/div[3]/div[3]/div[4]/div/table[4]') %>%
-  html_table(fill = TRUE)
-
-
-# Format the data
-library(lubridate)
-data_cleaned = raw_data[2:(nrow(raw_data)-2),1:(ncol(raw_data)-2)]
-colnames(data_cleaned) =raw_data[1,1:(ncol(raw_data)-2)] 
-
-data_final = t(data_cleaned[,2:ncol(data_cleaned)])
-colnames(data_final) = data_cleaned$Date
-
-# Fixing problems Dukes/Nantucket
-idx_dukes_nantucket = which(rownames(data_final)=="Dukes and Nantucket")
-## I save the data
-row = data_final[idx_dukes_nantucket,]
-## I change the name "Dukes and Nantucket" -> "Dukes"
-rownames(data_final)[idx_dukes_nantucket] = "Dukes"
-## I create the row for Nantucket
-data_final = rbind(data_final, row)
-rownames(data_final)[nrow(data_final)] = "Nantucket"
-
-# I change the names of the columns
-list_of_dates = as.Date(paste(colnames(data_final),"2020",sep=" "),"%B %d %Y")
-colnames(data_final)=format(list_of_dates, format="%Y-%m-%d")
-
-mat = data_final
-# I fill the empty cells and convert all cells to numeric
-for(r in 1:nrow(mat)){
-  current_vect = mat[r, ]
-  prev = 0
-  vect_results=c()
-  for(element in current_vect){
-    if(is.na(element)){
-      vect_results=c(vect_results,prev)
-    }else if(element==""){
-      vect_results=c(vect_results,prev)
-    }else{
-      vect_results=c(vect_results,as.numeric(element))
-      prev = as.numeric(element)
-    }
+if (file.exists("data/collection.Rdata")){
+  load("data/collection.Rdata")
+}
+  
+current_data_hash = digest(r_data_wiki, "md5")
+if(exists("stored_data_hash")){
+  if(current_data_hash != stored_data_hash){
+    get_data_from_wiki(r_data_wiki)
   }
-  mat[r,] = vect_results
-}
-m = sapply(mat, FUN=as.numeric)
-mat_final=matrix(m, ncol=ncol(data_final),nrow=nrow(data_final))
-
-# I convert the matrix to data frame
-df_final = as.data.frame(mat_final)
-rownames(df_final) = rownames(data_final)
-colnames(df_final) = colnames(data_final)
-
-# I add a column with the names
-df_final$NAME = rownames(df_final)
-df_final=df_final[,5:ncol(df_final)]
-names_counties = sort(c("All",df_final$NAME))
-write.csv(df_final,file="data/cases_by_county.csv",quote=FALSE,row.names=FALSE)
-
-date_latest_avail_data=colnames(df_final)[ncol(df_final)-1]
-
-
-# DATA FOR THE MAP
-df_leaflet = ma_spdf
-df_leaflet@data = left_join(df_leaflet@data, df_final,by = "NAME")
-
-
-# DATA FOR THE CURVE OF THE NUM OF DEATHS
-raw_data_table_deaths = raw_data_wiki %>% 
-  html_node(xpath = '/html/body/div[3]/div[3]/div[4]/div/table[3]') %>%
-  html_table(fill = TRUE)
-# location column of the deaths
-column_deaths = ncol(raw_data_table_deaths) - 2
-df_num_deaths = data.frame(Date=as.Date(paste(raw_data_table_deaths[7:(nrow(raw_data_table_deaths)-2),1],"2020",sep=" "), format = "%B %d %Y"),
-                                        Num_deaths = raw_data_table_deaths[7:(nrow(raw_data_table_deaths)-2),column_deaths],stringsAsFactors = FALSE)
-
-df_num_deaths = df_num_deaths %>% mutate(Num_deaths = ifelse(Num_deaths %in% "", 0, as.numeric(Num_deaths)))
-df_num_deaths[df_num_deaths$Date=="2020-03-20","Num_deaths"]=1
-total_num_deaths = sum(as.numeric(df_num_deaths$Num_deaths))
-ma_population = 6892503
-perc_entire_population = total_num_deaths/ma_population*100
-perc_vs_num_cases = total_num_deaths / (sum(df_final[df_final$NAME!="Nantucket",(ncol(df_final)-1)])) * 100
-
-df_to_plot_num_deaths = df_num_deaths %>% group_by(Date = cut(Date +6, "week", start.on.monday = FALSE)) %>% 
-  summarise(Num_deaths = sum(Num_deaths))
-
-df_to_plot_num_deaths$Date=as.Date(df_to_plot_num_deaths$Date)
-
-# remove last point if I do not have the data for the complete week
-if(as.vector(df_to_plot_num_deaths$Date)[nrow(df_to_plot_num_deaths)] < date_latest_avail_data){
-  df_to_plot_num_deaths = df_to_plot_num_deaths[1:(nrow(df_to_plot_num_deaths)-1),]
+  
+}else{
+  get_data_from_wiki(r_data_wiki)
 }
 
-
-
-
-
-# DATA FOR THE GROWTH CURVE OF THE CASES
-ma = colSums(df_final[,1:(ncol(df_final)-1)])
-# doubling every day
-n_0 = as.numeric(ma[5])
-#lambda=2
-#t=0:(nrow(df_final)-1)
-#n = n_0 * lambda^t
-# doubling every two days
-lambda=2
-t=0:(ncol(df_final)-6)
-n = n_0 * lambda^(t/3)
-doubl_3_days = c(rep(0,4),n)
-# doubling every three days
-lambda=2
-t=0:(ncol(df_final)-6)
-n = n_0 * lambda^(t/4)
-doubl_4_days = c(rep(0,4),n)
-
-df_to_plot=data.frame(
-  Date=as.Date(colnames(df_final[1:(ncol(df_final)-1)])),
-  Cases=ma)
-
-# DATA FOR THE EPI CURVE
-
-cases_by_county = df_final
-
-# sum the data for all the counties / I need to subtract the data of Dukes or Nuntacket because they are
-# duplicated in order to display the map correctly
-data_all_counties=c(colSums(cases_by_county[,1:(length(cases_by_county)-1)]) -
-                      as.numeric(cases_by_county[cases_by_county$NAME=="Dukes", 1:(length(cases_by_county)-1)]),
-                    "All")
-cases_by_county=rbind(cases_by_county,data_all_counties)
--- server.R
+++ server.R
@@ -1,16 +1,130 @@
 
 
-server = function(input, output) {
+server = function(input, output, session) {
   
-  output$leaflet_chloropleth = renderLeaflet({
-    
-    leaflet(df_leaflet) %>% 
-      addTiles()  %>% 
-      fitBounds(~min(-70), ~min(41.5), ~max(-73.5), ~max(43))
-  })
+   
+   output$leaflet_chloropleth = renderLeaflet({
+      
+      target_date = date_latest_avail_data
+   
+      mybins = c(0,1,10,100,1000,10000,100000)
+      mypalette = colorBin( palette="YlOrRd", domain=df_leaflet@data[,target_date], na.color="transparent", bins=mybins)
+      
+      mytag = paste(
+         "County: ", df_leaflet@data$NAME,"<br/>", 
+         "Cases: ", df_leaflet@data[,target_date], "<br/>", 
+         sep="") %>%
+         lapply(htmltools::HTML)
+      
+      leaflet(df_leaflet) %>% 
+         addTiles()  %>% 
+         fitBounds(~min(-70), ~min(41.5), ~max(-73.5), ~max(43)) %>%
+         clearShapes() %>%
+         addPolygons(data =df_leaflet, fillColor = ~mypalette(df_leaflet@data[,target_date]),
+                     fillOpacity = 0.5,
+                     stroke=TRUE,
+                     weight=0.8,
+                     color="#000000",
+                     label = mytag,
+                     labelOptions = labelOptions(
+                        style = list("font-weight" = "normal", padding = "3px 8px"),
+                        textsize = "10px",
+                        direction = "auto"
+                     ))%>% clearControls() %>%
+         addLegend( pal=mypalette, values=df_leaflet@data[,target_date], opacity=0.5, title = "Cases", position = "bottomleft" )
+      
+      
+   })
+   
+
+   output$curves = renderPlotly({
+      
+      
+      
+      choice = input$choose_type
+      if(choice == "Linear"){
+         p=ggplot(data=df_to_plot, aes(x=Date, y=Cases, group=1)) +
+            geom_line(size=1, color="#636363")+
+            geom_point(shape=19, size=2, color="#525252") +
+            xlab("Date") + 
+            ylab("Number of cases") +
+            scale_x_date(date_breaks = "1 week", date_labels = "%b %d")
+         
+      }else{
+         
+         p=ggplot(data=df_to_plot, aes(x=Date, y=Cases, group=1)) +
+            geom_line(size=1, color="#636363")+
+            geom_point(shape=19, size=2, color="#525252") +
+            xlab("Date") + 
+            ylab("Number of cases")+
+            scale_y_log10() + 
+            scale_x_date(date_breaks = "1 week", date_labels = "%b %d")
+      }
+      w =ggplotly(p + 
+                     geom_line(aes(x=Date, y = doubl_3_days), color = "red", linetype = "dotted") +
+                     geom_line(aes(x=Date, y = doubl_4_days), color = "orange", linetype = "dotted")
+                  
+      )
+      w %>% style(hoverinfo = "skip", traces = c(2,3))
+      
+   })
+   
+   
+   
+   output$epi_curve = renderPlotly({
+      
+      selected_county = input$choose_county
+      
+      
+      numbers = cases_by_county[cases_by_county$NAME==selected_county,]
+      
+      n_cases_selected_county=as.numeric(numbers[1:(length(numbers)-1)])
+      vect_for_diff=c(0,n_cases_selected_county[1:(length(n_cases_selected_county)-1)])
+      vect_new_cases = n_cases_selected_county - vect_for_diff
+      df_new_cases = data.frame(Date=as.Date(colnames(numbers)[1:(length(numbers)-1)]),
+                                New_cases=vect_new_cases)
+      df_to_plot_epi_curve = df_new_cases %>% group_by(Date = cut(Date +6, "week", start.on.monday = FALSE)) %>% 
+         summarise(New_cases = sum(New_cases))
+      
+      df_to_plot_epi_curve$Date=as.Date(df_to_plot_epi_curve$Date)
+      
+      # remove last point if I do not have the data for the complete week
+      if(as.vector(df_to_plot_epi_curve$Date)[nrow(df_to_plot_epi_curve)] < date_latest_avail_data){
+         df_to_plot_epi_curve = df_to_plot_epi_curve[1:(nrow(df_to_plot_epi_curve)-1),]
+      }
+      
+      p = ggplot(df_to_plot_epi_curve,aes(x=Date,y=New_cases))+
+         geom_area( fill="#de2d26", alpha=0.4) +
+         geom_line(color="#de2d26", size=1) +
+         geom_point(size=2, color="#de2d26") +
+         xlab("Week") + 
+         ylab("New cases")+
+         scale_x_date(date_breaks = "1 week", date_labels = "%b %d")
+      ggplotly(p)
+      
+   })
+   
+   
+   output$graph_deaths = renderPlotly({
+      
+      
+      p = ggplot(df_to_plot_num_deaths,aes(x=Date,y=Num_deaths))+
+         geom_area( fill="#252525", alpha=0.4) +
+         geom_line(color="#252525", size=1) +
+         geom_point(size=2, color="#252525") +
+         xlab("Week") + 
+         ylab("Number of deaths")+
+         scale_x_date(date_breaks = "1 week", date_labels = "%b %d")
+      ggplotly(p)
+      
+   })
+   
+   
   
    observe({
-     target_date = req(as.character(input$date))
+      
+      
+     target_date = as.character(input$date)
      
      mybins = c(0,1,10,100,1000,10000,100000)
      mypalette = colorBin( palette="YlOrRd", domain=df_leaflet@data[,target_date], na.color="transparent", bins=mybins)
@@ -41,86 +155,6 @@ server = function(input, output) {
    } )
    
 
-   output$curves = renderPlotly({
-      
-      
-     
-     choice = input$choose_type
-     if(choice == "Linear"){
-       p=ggplot(data=df_to_plot, aes(x=Date, y=Cases, group=1)) +
-         geom_line(size=1, color="#636363")+
-         geom_point(shape=19, size=2, color="#525252") +
-         xlab("Date") + 
-         ylab("Number of cases") +
-         scale_x_date(date_breaks = "1 week", date_labels = "%b %d")
 
-     }else{
-       
-       p=ggplot(data=df_to_plot, aes(x=Date, y=Cases, group=1)) +
-         geom_line(size=1, color="#636363")+
-         geom_point(shape=19, size=2, color="#525252") +
-         xlab("Date") + 
-         ylab("Number of cases")+
-         scale_y_log10() + 
-         scale_x_date(date_breaks = "1 week", date_labels = "%b %d")
-         }
-     w =ggplotly(p + 
-                   geom_line(aes(x=Date, y = doubl_3_days), color = "red", linetype = "dotted") +
-                   geom_line(aes(x=Date, y = doubl_4_days), color = "orange", linetype = "dotted")
-                 
-     )
-     w %>% style(hoverinfo = "skip", traces = c(2,3))
-     
-   })
-  
-   
-   
-   output$epi_curve = renderPlotly({
-      
-      selected_county = input$choose_county
-      
-
-      numbers = cases_by_county[cases_by_county$NAME==selected_county,]
-         
-      n_cases_selected_county=as.numeric(numbers[1:(length(numbers)-1)])
-      vect_for_diff=c(0,n_cases_selected_county[1:(length(n_cases_selected_county)-1)])
-      vect_new_cases = n_cases_selected_county - vect_for_diff
-      df_new_cases = data.frame(Date=as.Date(colnames(numbers)[1:(length(numbers)-1)]),
-                                New_cases=vect_new_cases)
-      df_to_plot_epi_curve = df_new_cases %>% group_by(Date = cut(Date +6, "week", start.on.monday = FALSE)) %>% 
-         summarise(New_cases = sum(New_cases))
-      
-      df_to_plot_epi_curve$Date=as.Date(df_to_plot_epi_curve$Date)
-      
-      # remove last point if I do not have the data for the complete week
-      if(as.vector(df_to_plot_epi_curve$Date)[nrow(df_to_plot_epi_curve)] < date_latest_avail_data){
-         df_to_plot_epi_curve = df_to_plot_epi_curve[1:(nrow(df_to_plot_epi_curve)-1),]
-      }
-      
-      p = ggplot(df_to_plot_epi_curve,aes(x=Date,y=New_cases))+
-         geom_area( fill="#de2d26", alpha=0.4) +
-         geom_line(color="#de2d26", size=1) +
-         geom_point(size=2, color="#de2d26") +
-         xlab("Week") + 
-         ylab("New cases")+
-         scale_x_date(date_breaks = "1 week", date_labels = "%b %d")
-      ggplotly(p)
-      
-   })
-   
-   
-   output$graph_deaths = renderPlotly({
-      
-     
-      p = ggplot(df_to_plot_num_deaths,aes(x=Date,y=Num_deaths))+
-         geom_area( fill="#252525", alpha=0.4) +
-         geom_line(color="#252525", size=1) +
-         geom_point(size=2, color="#252525") +
-         xlab("Week") + 
-         ylab("Number of deaths")+
-         scale_x_date(date_breaks = "1 week", date_labels = "%b %d")
-      ggplotly(p)
-      
-   })
     
 }
